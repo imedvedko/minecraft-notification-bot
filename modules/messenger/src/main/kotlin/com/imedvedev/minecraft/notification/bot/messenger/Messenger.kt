5c576848c0fc54209d2261badcac19fc154b4c20
Ilya Medvedev <medved55rus@gmail.com>
2019-10-12 17:19:03 +0000

    Integrate AuthMe events

--- modules/messenger/src/main/kotlin/com/imedvedev/minecraft/notification/bot/messenger/Messenger.kt
+++ modules/messenger/src/main/kotlin/com/imedvedev/minecraft/notification/bot/messenger/Messenger.kt
@@ -1,5 +1,7 @@
 package com.imedvedev.minecraft.notification.bot.messenger
 
+import kotlinx.coroutines.Job
+
 interface Messenger {
-    fun send(message: String, onlinePlayer: List<String>)
+    fun send(message: String): List<Job>
 }
